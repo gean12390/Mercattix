7620f22eb48394221c4b51ced15d2fb0805ae086
juantf16 <juan16foltran@gmail.com>
2025-07-17 16:59:13 -0300

    começo da rota post para forms de contato

--- index.js
+++ index.js
@@ -2,24 +2,15 @@
 const express = require ('express');
 const app = express ();
 const PORT = 3000;
+const rotas = require('./routes.js');
 const path = require('path');
-const fs = require('fs')
 
+app.use(rotas)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    try {
-        const html = fs.readFileSync(path.join(__dirname, './public/html/index.html'), 'utf8');
-        res.send(html);
-    } catch (error) {
-        console.error('Erro ao carregar o index.html:', error);
-        res.status(500).send('Erro interno no servidor.');
-    }
-});
-
 // Iniciando servidor
 app.listen (PORT, () => {
     console.log (`Servidor rodando na porta ${PORT}, acesse aqui http://localhost:${PORT}`);
-});
+}); 
